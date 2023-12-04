f788efd15bdfed628526dfbf3db052d026ed4943
miyachun <miyachun808@gmail.com>
2023-12-04 15:13:05 +0800

    first commit

---
+++ api/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,request
+import json,urllib.request
+import random
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    
+    f1=[]
+    f2=[]    
+    url ="https://www.desen.com.tw/sample.json"
+    data = urllib.request.urlopen(url).read()
+    output = json.loads(data)
+    if request.method=='POST':
+        btA=request.values.get('myA')
+        btB=request.values.get('myB')
+        if btA=='myA':
+           f1.clear()
+           f2.clear()
+           Rr=random.randrange(0, 89, 1)
+           f1.append(output[Rr]['name'])
+           f2.append(output[Rr]['d1'])
+        if btB=='myB':
+            f1.clear()
+            f2.clear()
+            return render_template('index.html',dF1=f1,dF2=f2)    
+    return render_template('index.html',dF1=f1,dF2=f2)
+
+if __name__ == '__main__':
+    app.run()
