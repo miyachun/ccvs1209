8240547624cbe233112fa1f22e3bb6bfd4403987
miyachun <miyachun808@gmail.com>
2023-12-06 20:15:43 +0800

    first commit

--- api/app.py
+++ api/app.py
@@ -2,29 +2,35 @@ from flask import Flask, render_template,request
 import json,urllib.request
 import random
 app = Flask(__name__, static_url_path='/static')
-
+Rr=-100
+Rr01=-100
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    global Rr
+    global Rr01
+
+    Qu=''
+    As=''    
+    url ="https://ccvs1209.vercel.app/static/song.json"    
     
-    f1=[]
-    f2=[]    
-    url ="https://ccvs1209.vercel.app/static/sample.json"
     data = urllib.request.urlopen(url).read()
     output = json.loads(data)
     if request.method=='POST':
-        btA=request.values.get('myA')
-        btB=request.values.get('myB')
-        if btA=='myA':
-           f1.clear()
-           f2.clear()
+        btQu=request.values.get('myQu')
+        btAns=request.values.get('myAns')        
+        if btQu=='myQu':
            Rr=random.randint(0,89)
-           f1.append(output[Rr]['name'])
-           f2.append(output[Rr]['d1'])
-        if btB=='myB':
-            f1.clear()
-            f2.clear()
-            return render_template('index.html',dF1=f1,dF2=f2)    
-    return render_template('index.html',dF1=f1,dF2=f2)
+           Rr01=111
+           Qu=output[Rr]['name']
+           As=output[Rr]['d1']
+           return render_template('index.html',Qu=Qu,As=As,Rr01=Rr01)            
+        if btAns=='myAns':
+            Rr01=100
+            Qu=output[Rr]['name']
+            As=output[Rr]['d1']             
+            return render_template('index.html',Qu=Qu,As=As,Rr01=Rr01)   
+
+    return render_template('index.html',Qu=Qu,As=As,Rr01=Rr01)
 
 if __name__ == '__main__':
     app.run()
